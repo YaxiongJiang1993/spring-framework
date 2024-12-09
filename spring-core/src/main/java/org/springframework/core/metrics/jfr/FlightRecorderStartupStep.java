6cfa43b073444ce4abc141bfe64d16e8d857df34
David Jiang <david.jiang@bitkernel.tech>
2024-12-09 15:18:41 +0800

    remove jdk11 event

--- spring-core/src/main/java/org/springframework/core/metrics/jfr/FlightRecorderStartupStep.java
+++ spring-core/src/main/java/org/springframework/core/metrics/jfr/FlightRecorderStartupStep.java
@@ -43,7 +43,7 @@ class FlightRecorderStartupStep implements StartupStep {
 			Consumer<FlightRecorderStartupStep> recordingCallback) {
 
 		this.event = new FlightRecorderStartupEvent(id, name, parentId);
-		this.event.begin();
+//		this.event.begin();
 		this.recordingCallback = recordingCallback;
 	}
 
@@ -82,7 +82,7 @@ class FlightRecorderStartupStep implements StartupStep {
 
 	@Override
 	public void end() {
-		this.event.end();
+		/*this.event.end();
 		if (this.event.shouldCommit()) {
 			StringBuilder builder = new StringBuilder();
 			this.tags.forEach(tag ->
@@ -91,7 +91,7 @@ class FlightRecorderStartupStep implements StartupStep {
 			this.event.setTags(builder.toString());
 		}
 		this.event.commit();
-		this.recordingCallback.accept(this);
+		this.recordingCallback.accept(this);*/
 	}
 
 	protected FlightRecorderStartupEvent getEvent() {
